d617ca1b21d37e9bd7c0a1c06bc53cd8f2412ab8
Joss <Joss@192.168.1.13>
2018-08-17 23:36:42 +0700

    doi sang cach lay du lieu tu tap tin .csv

--- src/ImportData.java
+++ src/ImportData.java
@@ -3,41 +3,13 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-class Attribute{
-	private String name;
-	private List<String> domain_value;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public List<String> getDomainValue() {
-		return domain_value;
-	}
-	public void setDomainValue(List<String> domain_value) {
-		this.domain_value = domain_value;
-	}
-	public Attribute(String name,String domain_value) {
-		this.name = name;
-		this.domain_value = new ArrayList<>();
-		if(domain_value.indexOf("{")>=0) {
-			domain_value = domain_value.substring(1,domain_value.length()-1);
-			for (String string : domain_value.split(",")) {
-				this.domain_value.add(string);
-			}
-		}
-		//System.out.println(name +" - "+domain_value);
-	}
-}
-
 class Data{
-	private List<Attribute> attribute;
+	private List<String> attribute;
 	private List<String> value;
-	public List<Attribute> getAttribute() {
+	public List<String> getAttribute() {
 		return attribute;
 	}
-	public void setAttribute(List<Attribute> attribute) {
+	public void setAttribute(List<String> attribute) {
 		this.attribute = attribute;
 	}
 	public List<String> getValue() {
@@ -46,13 +18,13 @@ class Data{
 	public void setValue(List<String> value) {
 		this.value = value;
 	}
-	public Data(List<Attribute> _attribute,List<String> _data) {
+	public Data(List<String> _attribute,List<String> _data) {
 		this.attribute = _attribute;
 		this.value = _data;
 	}
 	public String convertAttributeName(int index) {
 		if(index < 0 || index >= this.attribute.size()) return "";
-		return this.attribute.get(index).getName();
+		return this.attribute.get(index);
 	}
 }
 
@@ -60,19 +32,17 @@ public class ImportData {
 
 	public static Data getDataFromFile(String file_in_name) {
 		try {
-			List<Attribute> list_att = new ArrayList<>();
+			List<String> list_att = new ArrayList<>();
 			List<String> list_value = new ArrayList<>();
 			BufferedReader reader = new BufferedReader(new FileReader(file_in_name));
 			String receive = reader.readLine();
-			Boolean got_data =  false;
-			while (receive!=null) {
-				if(receive.indexOf("@attribute")>=0) {
-					list_att.add(new Attribute(receive.split(" ")[1], receive.split(" ")[2]));
-				}
-				else if(receive.indexOf("@data")>=0) got_data = true;
-				else if(got_data) {
+			
+			for (String string : receive.split(",")) list_att.add(string);
+			
+			receive = reader.readLine();
+			while (receive != null) {
+				if(receive.length()> 1) {
 					list_value.add(receive);
-					//System.out.println(receive);
 				}
 				receive = reader.readLine();
 			}
--- src/main.java
+++ src/main.java
@@ -2,7 +2,9 @@
 public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Data _data = ImportData.getDataFromFile("letter-train.arff");
+		System.out.println("start");
+		Data _data = ImportData.getDataFromFile("zoo.arff");
+		//for (String string : _data.getValue()) System.out.println(string);
 		
 		Process.createTreeID3(_data).outputID3(_data);;
 	}
